d23a741afef2c7a85d2513465c354a61bdc6b7f6
Brandon Maxfield <61363181+Bmaxfi1@users.noreply.github.com>
2020-04-23 15:38:01 -0600

    first

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace first_csharp_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int i = 1;
+            while (i <= 5)
+            {
+                Console.WriteLine("i is equal to " + i);
+                i++;
+            }
+        }
+    }
+}
